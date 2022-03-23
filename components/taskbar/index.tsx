f3854628894764f8aa5730b371162816938779d9
Francois Mahieu <francois.mahieu16@gmail.com>
2022-03-22 17:12:12 -0700

    Refactors ShortcutBar

--- components/taskbar/index.tsx
+++ components/taskbar/index.tsx
@@ -1,8 +1,6 @@
-import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
-import { FcApproval, FcApprove, FcAreaChart, FcBiotech, FcBookmark, FcCamera, FcClapperboard, FcGlobe } from 'react-icons/fc';
 import { Component } from 'react';
 import Image from 'next/image';
-import TaskbarShortcut from './shortcut';
+import ShortcutBar from './shortcut-bar';
 import styles from './taskbar.module.scss';
 
 class Taskbar extends Component {
@@ -12,30 +10,11 @@ class Taskbar extends Component {
 
   state = {
     date: '',
-    shortcutIems: [
-        (<FcAreaChart size={28} key={0}/>),
-        (<FcBookmark size={28} key={1}/>),
-        (<FcApproval size={28} key={2}/>),
-        (<FcApprove size={28} key={3}/>),
-        (<FcBiotech size={28} key={4}/>),
-        (<FcCamera size={28} key={5}/>),
-        (<FcClapperboard size={28} key={6}/>),
-        (<FcGlobe size={28} key={7}/>)
-    ],
     time: ''
   };
 
   componentDidMount() {
     this.updateDateTime();
-  }
-
-  handleReorderShortcutItemsOnDragEnd = (result: DropResult) => {
-    if (!result.destination) return;
-
-    const items = Array.from(this.state.shortcutIems);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    this.setState({ shortcutIems: items });
   }
 
   render() {
@@ -45,25 +24,13 @@ class Taskbar extends Component {
           <Image src='/taskbar/windows-logo.png' alt='menu' height={24} width={24}/>
         </div>
 
-          {/* User icons */}
-          <DragDropContext onDragEnd={this.handleReorderShortcutItemsOnDragEnd}>
-            <Droppable droppableId="taskbarShortcutItems" direction="horizontal">
-              {
-                (provided: any) => (
-                  <div className={styles.shortcuts} ref={provided.innerRef} {...provided.droppableProps}>
-                    { this.getFakeIconList() }
-                    {provided.placeholder}
-                  </div>
-                )
-              }
-            </Droppable>
-          </DragDropContext>
+        <ShortcutBar/>
 
-          {/* Time section */}
-          <div className={styles.datetime}>
-            <div>{this.state.time}</div>
-            <div>{this.state.date}</div>
-          </div>
+        {/* Time section */}
+        <div className={styles.datetime}>
+          <div>{this.state.time}</div>
+          <div>{this.state.date}</div>
+        </div>
       </div>
     );
   }
@@ -92,22 +59,6 @@ class Taskbar extends Component {
   formatDate(date: Date): string {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   }
-
-  getFakeIconList = () => this.state.shortcutIems.map((item: any, i) => (
-      <Draggable key={i} draggableId={String(i)} index={i}>
-        {
-          (provided: any) => (
-            <TaskbarShortcut>
-              <div className={styles.shortcutItem} ref={provided.innerRef}
-                {...provided.draggableProps} {...provided.dragHandleProps}
-              >
-                {item}
-              </div>
-            </TaskbarShortcut>
-          )
-        }
-      </Draggable>
-    ));
 }
 
 export default Taskbar;
