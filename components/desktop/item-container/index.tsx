505f28edd2d8618e99281f0a489b6cecd12415a2
francoismahieu <francois.mahieu16@gmail.com>
2022-03-25 22:20:43 -0700

    Begging of selection box

--- components/desktop/item-container/index.tsx
+++ components/desktop/item-container/index.tsx
@@ -15,21 +15,20 @@ export type DesktopItem = {
 
 type SelectionBox = {
   active: boolean,
-  top: number,
-  left: number,
   startX: number,
-  startY: number
+  startY: number,
+  width: number,
+  height: number
 }
 
 const startSelectionBox: SelectionBox = {
   active: false,
-  left: 0,
   startX: 0,
   startY: 0,
-  top: 0
+  height: 0,
+  width: 0
 };
 
-// TODO: should use a canvas?
 const DesktopItemContainer: FC<{ files: ExplorerFile[] }> = ({ files }) => {
   const [desktopItems, setDesktopItems] = useState<DesktopItem[]>([]);
   const [selectionBox, setSelectionBox] = useState<SelectionBox>(startSelectionBox);
@@ -41,18 +40,16 @@ const DesktopItemContainer: FC<{ files: ExplorerFile[] }> = ({ files }) => {
   }, []);
 
   // For selecting multiple elements by dragging
-    // ERROR: NOT GETTING UPDATED SELECTION BOX BECAUSE INDSIDE USE EFFECT()
+  // ERROR: NOT GETTING UPDATED SELECTION BOX BECAUSE INDSIDE USE EFFECT()
   useEffect(() => {
-    let savedMouseX = 0;
-    let savedMouseY = 0;
-
     const onMouseDown = (event: any) => {
       // TODO: change to check it's over anything other than 'desktop'?
+      console.log(`${event.clientY}, ${document.body.clientWidth}`)
       if (!isMouseOverItem(event.clientX, event.clientY, desktopItems)) {
          setSelectionBox({
           ...selectionBox,
           active: true,
-          startX: +event.clientX,
+          startX: window.innerWidth - +event.clientX,
           startY: +event.clientY
         });
       }
@@ -62,25 +59,38 @@ const DesktopItemContainer: FC<{ files: ExplorerFile[] }> = ({ files }) => {
       setSelectionBox({ ...selectionBox, active: false });
     }
 
-    const onMouseMove = (event: any) => {
-      if (selectionBox.active) {
-        savedMouseX = event.clientX;
-        savedMouseY = event.clientY;
-
-        setSelectionBox({ ...selectionBox, startX: savedMouseX, startY: savedMouseY });
-      }
-    };
-
     document.addEventListener('mousedown', onMouseDown);
     document.addEventListener('mouseup', onMouseUp);
-    document.addEventListener('mousemove', onMouseMove);
 
     return () => {
       document.removeEventListener('mousedown', onMouseDown);
       document.removeEventListener('mouseup', onMouseUp);
-      document.removeEventListener('mousemove', onMouseMove);
     };
   }, [desktopItems]);
+
+  useEffect(() => {
+    console.log(selectionBox)
+    const mousemove = (event: any) => {
+      const width = Math.abs(selectionBox.startX - +event.clientX);
+      const height = Math.abs(selectionBox.startY - +event.clientY);
+
+      //console.log(width + ' ' + height)
+      setSelectionBox({
+        ...selectionBox,
+        width, height
+      });
+
+      console.log(selectionBox);
+    }
+
+    if (selectionBox.active) {
+      document.addEventListener('mousemove', mousemove);
+
+      return () => {
+        document.removeEventListener('mousemove', mousemove);
+      };
+    }
+  }, [selectionBox]);
 
   const moveItem = (itemId: string, top: number, left: number) => {
     const element = desktopItems.find(el => el.name === itemId);
@@ -96,19 +106,21 @@ const DesktopItemContainer: FC<{ files: ExplorerFile[] }> = ({ files }) => {
   };
 
   const getSelectionBoxClassName = (): string => {
+    console.log('redrawing')
     return selectionBox.active ? styles.selectionBoxOn : styles.selectionBoxOff;
   };
 
   return (
     <Fragment>
-      { desktopItems.map((el, index) =>
+      { desktopItems.map((item, index) =>
         (
-          <DesktopItemComponent key={index} item={el} moveItem={moveItem}/>
+          <DesktopItemComponent key={index} item={item} moveItem={moveItem}/>
         )
       )}
 
-      <div style={{ left: selectionBox.startX, top: selectionBox.startY }}
-      className={getSelectionBoxClassName()}></div>
+      <div style={{ top: selectionBox.startY, right: selectionBox.startX,
+        width: selectionBox.width, height: selectionBox.height}}
+      className={styles.selectionBoxOn}></div>
     </Fragment>
   );
 };
