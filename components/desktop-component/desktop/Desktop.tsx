2cb423c9e712ec0b1f12be88940c9775a0d4fe3f
Francois Mahieu <francois.mahieu16@gmail.com>
2023-02-15 21:59:51 -0800

    Fix background image

--- components/desktop-component/desktop/Desktop.tsx
+++ components/desktop-component/desktop/Desktop.tsx
@@ -2,10 +2,12 @@ import { FC, Fragment } from 'react';
 import DesktopItemContainer from '../item-container/ItemsContainer';
 import styles from './desktop.module.scss';
 import { File } from '../../../System/file';
+import background from '../../../assets/background_image_light.jpg';
 
 const Desktop: FC = () => {
   return (
     <Fragment>
+      <img alt="background image" className={styles.backgroundImg} src={background.src}/>I
       <div className={styles.background} id='desktop'>
         <DesktopItemContainer files={File.getRoot().children[0].children}/>
       </div>
