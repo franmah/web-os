06c258c80c3ce16bdba3241f8e4543b99d410bb6
Francois Mahieu <francois.mahieu16@gmail.com>
2023-02-15 21:04:51 -0800

    Adds background image (not sized to screen)

--- components/desktop-component/desktop/Desktop.tsx
+++ components/desktop-component/desktop/Desktop.tsx
@@ -1,13 +1,16 @@
-import { FC } from 'react';
+import { FC, Fragment } from 'react';
 import DesktopItemContainer from '../item-container/ItemsContainer';
 import styles from './desktop.module.scss';
 import { File } from '../../../System/file';
 
 const Desktop: FC = () => {
   return (
-    <div className={styles.background} id='desktop'>
-      <DesktopItemContainer files={File.getRoot().children[0].children}/>
-    </div>
+    <Fragment>
+      <div className={styles.background} id='desktop'>
+        <DesktopItemContainer files={File.getRoot().children[0].children}/>
+      </div>
+    </Fragment>
+    
   );
 };
 
