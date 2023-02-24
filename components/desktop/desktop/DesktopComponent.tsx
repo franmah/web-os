d310f0b26e0ffc64477a57b32370bd8e8deab94d
Francois Mahieu <francois.mahieu16@gmail.com>
2023-02-23 16:54:52 -0800

    Refactor FileSystemContext

--- components/desktop/desktop/DesktopComponent.tsx
+++ components/desktop/desktop/DesktopComponent.tsx
@@ -1,13 +1,15 @@
-import { FC, Fragment } from 'react';
+import { FC, Fragment, useContext } from 'react';
 import DesktopItemContainerComponent from '../items-container/ItemsContainerComponent';
 import styles from './desktop.module.scss';
-import { File } from '../../../System/file/file';
 import background from '../../../assets/background_image_light.jpg';
 import background_1080 from '../../../assets/background_image_light_1080_2.jpg';
 import background_1440 from '../../../assets/background_image_light_1440_2.jpg';
 import background_2400 from '../../../assets/background_image_light_2400_2.jpg';
+import { FileSystemContext } from '../../../contexts/FileSystemContext';
 
 const Desktop: FC = () => {
+  const { getDesktop } = useContext(FileSystemContext);
+
   return (
     <Fragment>
       <img
@@ -18,7 +20,7 @@ const Desktop: FC = () => {
       />
       
       <div className={styles.background} id='desktop'>
-        <DesktopItemContainerComponent files={File.getRoot().children[0].children}/>
+        <DesktopItemContainerComponent files={getDesktop().children}/>
       </div>
     </Fragment>
   );
---
+++ hooks/useFileSystemContextState.ts
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { v4 } from "uuid";
+import { getExampleDesktopChildren } from "../services/FileSystemService";
+import { ExplorerFile } from "../types/ExplorerElement";
+
+export const useFileSystemContextState = () => {
+  const rootFile: ExplorerFile = {
+    name: 'root',
+    id: 'root',
+    children: [],
+    parent: null
+  };
+
+  rootFile.children.push({
+    name: 'Deskop',
+    id: 'desktop',
+    children: [],
+    parent: rootFile
+  });
+
+  rootFile.children.push({
+    name: 'Document',
+    id: 'document',
+    children: [],
+    parent: rootFile
+  });
+
+  rootFile.children[0].children = getExampleDesktopChildren(rootFile.children[0]);
+
+  const [root, setRoot] = useState<ExplorerFile>(rootFile);
+
+  const addFile = (name: string, iconPath: string, parent: ExplorerFile | null) => {
+      setRoot(root => {
+
+        const file = {
+          name,
+          iconPath,
+          parent,
+          children: [],
+          id: v4()
+        };
+    
+        if (parent)
+          parent.children.push(file);
+        
+        return root;
+      });
+  };
+
+  const getDesktop = (): ExplorerFile => {
+    return root.children[0];
+  };
+
+  return { root, addFile, getDesktop };
+}
