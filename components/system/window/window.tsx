5a0f1a440dfe9ad05d2a7cb1b46248958fc5f42c
Francois Mahieu <francois.mahieu16@gmail.com>
2023-03-04 22:28:18 -0800

    Fix window resize. Maximize accounts for taskbar

--- components/system/window/window.tsx
+++ components/system/window/window.tsx
@@ -1,4 +1,5 @@
 import { FC, useEffect, useState } from "react";
+import { TASKBAR_HEIGHT } from "../../../constants/TaskbarConsts";
 import styles from './window.module.scss';
 
 // TODO: move to its own folder
@@ -10,7 +11,11 @@ export type WindowState = {
   moving: boolean,
   resizing: boolean,
   previousClientX: number,
-  previousClientY: number
+  previousClientY: number,
+  previousTop: number,
+  previousLeft: number,
+  previousWidth: number,
+  previousHeight: number,
 };
 
 const DEFAULT_WINDOW_STATE: WindowState = {
@@ -21,9 +26,25 @@ const DEFAULT_WINDOW_STATE: WindowState = {
   moving: false,
   resizing: false,
   previousClientX: 0,
-  previousClientY: 0
+  previousClientY: 0,
+  previousTop: 150,
+  previousLeft: 150,
+  previousWidth: 400,
+  previousHeight: 400
 };
 
+/**
+ * ISSUE:
+ * When window is in fullscreen, the previous position is saved.
+ * When double clicking, it triggers a mouse down that will set the options to resize/move
+ * which then sets the previous position (stopMovingAndResizingWindow) to full
+ * 
+ * See this for how to use event.detail + timeout
+ * https://stackoverflow.com/a/60177326
+ * onMouseDown functions whould have to setTimeout before moving/resizing to make sure it's not a double click
+ * 
+ * Or maybe use on drag start
+ */
 const WindowComponent: FC<{ children: React.ReactNode }> = ({ children }) => {
 
   const [options, setOptions] = useState<WindowState>(DEFAULT_WINDOW_STATE);
@@ -44,14 +65,27 @@ const WindowComponent: FC<{ children: React.ReactNode }> = ({ children }) => {
   }, []);
 
   const stopMogingAndResizingWindow = () => {
-    setOptions(options => ({
-      ...options,
-      moving: false,
-      resizing: false
-    }));
+    setOptions(options => {
+      console.log('setting up previous: saving: ' + (options.moving))
+      if (options.resizing || options.moving) {
+        return {
+          ...options,
+          moving: false,
+          resizing: false,
+          previousTop: options.top,
+          previousLeft: options.left,
+          previousHeight: options.height,
+          previousWidth: options.width
+        }
+      } else {
+        console.log('NOT setting up previous')
+        return options;
+      }
+    });
   };
 
   const onHeaderMouseDown = (event: any) => {
+    console.log('on mouse down: setting moving to true')
     setOptions(state => ({
       ...state,
       moving: true,
@@ -61,6 +95,7 @@ const WindowComponent: FC<{ children: React.ReactNode }> = ({ children }) => {
   };
 
   const onBordersMouseDown = (event: any) => {
+    console.log('border mouse down')
     setOptions(state => ({
       ...state,
       resizing: true,
@@ -71,6 +106,7 @@ const WindowComponent: FC<{ children: React.ReactNode }> = ({ children }) => {
 
   const onMouseMove = (event: MouseEvent) => {
     setOptions(options => {
+      console.log(options.resizing)
       if (options.moving) {
         return moveWindow(event, options);
       } else if (options.resizing) {
@@ -80,23 +116,23 @@ const WindowComponent: FC<{ children: React.ReactNode }> = ({ children }) => {
     });
   };
 
-  const resizeWindow = (event: any, options: WindowState) => {
+  const resizeWindow = (event: any, options: WindowState): WindowState => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
     const changeX = mouseX - options.previousClientX;
     const changeY = mouseY - options.previousClientY;
-
+    console.log(changeX)
     return {
       ...options,
       width: options.width + changeX,
       height: options.height + changeY,
       previousClientX: mouseX,
-      previousClientY: mouseY
+      previousClientY: mouseY,
     }
   };
 
-  const moveWindow = (event: any, options: WindowState) => {
+  const moveWindow = (event: any, options: WindowState): WindowState => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
@@ -107,8 +143,27 @@ const WindowComponent: FC<{ children: React.ReactNode }> = ({ children }) => {
       top: options.top + changeY,
       left: options.left + changeX,
       previousClientX: mouseX,
-      previousClientY: mouseY
+      previousClientY: mouseY,
     }
+  };
+
+  const maximizeWindow = (event: any) => {
+    console.log('double click')
+    setOptions(options => {
+      const isAlreadyMaximized = 
+        options.height === window.innerHeight - TASKBAR_HEIGHT &&
+        options.width === window.innerHeight &&
+        options.top === 0 &&
+        options.left === 0;
+
+      return {
+        ...options,
+        top: isAlreadyMaximized ? options.previousTop : 0,
+        left: isAlreadyMaximized ? options.previousLeft : 0,
+        width: isAlreadyMaximized ? options.previousWidth : window.innerWidth,
+        height: isAlreadyMaximized ? options.previousHeight : window.innerHeight - TASKBAR_HEIGHT
+      }
+    });
   };
 
   return (
@@ -118,16 +173,17 @@ const WindowComponent: FC<{ children: React.ReactNode }> = ({ children }) => {
       style={{
         top: options.top,
         left: options.left,
-        width: options.width,
-        height: options.height
+        width: `${options.width}px`,
+        height: `${options.height}px`
       }}
     >
 
       <div
         onMouseDown={onHeaderMouseDown}
+        onDoubleClick={maximizeWindow}
+        onDragCapture={() => console.log("drag start")}
         className={styles.header}
       >
-
       </div>
 
       <div className={styles.childrenContainer}>
