bc1a10dd6481f2c03006826cbd681fc473026e6f
Francois Mahieu <francois.mahieu16@gmail.com>
2023-03-08 19:10:30 -0800

    Refactor constants

--- animations/windowMaximizeAnimations.ts
+++ animations/windowMaximizeAnimations.ts
@@ -1,11 +1,15 @@
+import { TASKBAR_HEIGHT } from "../constants/TaskbarConsts";
+
+const HEIGHT_OFFSET = '15px';
+
 export const maximizeAnimation = {
   name: 'window-maximize-animation',
   animation: (left: number, width: number, height: number) => {
     return `
       @keyframes window-maximize-animation {
         0%   {top: ${0}px; left: ${left}px; width: ${width}px; height: ${height}px;}
-        95%   {top: ${0}px; left: ${8}px; width: calc(100% - 16px); height: calc(100% - 65px);}
-        100% {top: ${8}px; left: ${8}px; width: calc(100% - 16px); height: calc(100% - 65px);}
+        95%   {top: ${0}px; left: ${8}px; width: calc(100% - 16px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
+        100% {top: ${8}px; left: ${8}px; width: calc(100% - 16px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
       }
     `;
   }
@@ -17,8 +21,8 @@ export const leftMaximizeAnimation = {
     return `
       @keyframes window-left-maximize-animation {
         0%   {top: ${top}px; left: ${0}px; width: ${0}px; height: ${height}px;}
-        90%   {top: ${8}px; left: ${0}px; width: calc(100% / 2 - 8px); height: calc(100% - 65px);}
-        100% {top: ${8}px; left: ${8}px; width: calc(100% / 2 - 8px); height: calc(100% - 65px);}
+        90%   {top: ${8}px; left: ${0}px; width: calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
+        100% {top: ${8}px; left: ${8}px; width: calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
       }
     `;
   }
@@ -30,8 +34,8 @@ export const rightMaximizeAnimation = {
     return `
       @keyframes window-right-maximize-animation {
         0%   {top: ${top}px; right: ${0}px; width: ${0}px; height: ${height}px;}
-        90%   {top: ${8}px; right: ${8}px; width: calc(100% / 2 - 8px); height: calc(100% - 65px);}
-        100% {top: ${8}px; right: ${8}px; width:calc(100% / 2 - 8px); height: calc(100% - 65px);}
+        90%   {top: ${8}px; right: ${8}px; width: calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
+        100% {top: ${8}px; right: ${8}px; width:calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
       }
     `;
   }
