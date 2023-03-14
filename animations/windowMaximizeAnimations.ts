441ef055320b8ba0ab9024f5c503a5ca5cbd187b
Francois Mahieu <francois.mahieu16@gmail.com>
2023-03-14 14:27:06 -0700

    Update animations to fit windows 11 style

--- animations/windowMaximizeAnimations.ts
+++ animations/windowMaximizeAnimations.ts
@@ -8,7 +8,6 @@ export const maximizeAnimation = {
     return `
       @keyframes window-maximize-placeholder-animation {
         0%   {top: ${0}px; left: ${left}px; width: ${width}px; height: ${height}px;}
-        95%   {top: ${0}px; left: ${8}px; width: calc(100% - 16px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
         100% {top: ${8}px; left: ${8}px; width: calc(100% - 16px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
       }
     `;
@@ -21,7 +20,6 @@ export const leftMaximizeAnimation = {
     return `
       @keyframes window-left-maximize-placeholder-animation {
         0%   {top: ${top}px; left: ${0}px; width: ${0}px; height: ${height}px;}
-        90%   {top: ${8}px; left: ${0}px; width: calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
         100% {top: ${8}px; left: ${8}px; width: calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
       }
     `;
@@ -34,7 +32,6 @@ export const rightMaximizeAnimation = {
     return `
       @keyframes window-right-maximize-placeholder-animation {
         0%   {top: ${top}px; right: ${0}px; width: ${0}px; height: ${height}px;}
-        90%   {top: ${8}px; right: ${8}px; width: calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
         100% {top: ${8}px; right: ${8}px; width:calc(100% / 2 - 8px); height: calc(100% - ${TASKBAR_HEIGHT}px - ${HEIGHT_OFFSET});}
       }
     `;
