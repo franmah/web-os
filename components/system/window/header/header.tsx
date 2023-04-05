1f58eb8a8bc8de8e0d339ea86de6d874326e839c
Francois Mahieu <francois.mahieu16@gmail.com>
2023-04-04 19:24:41 -0700

    Refactor maximize component out of header

--- components/system/window/header/header.tsx
+++ components/system/window/header/header.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import styles from './header.module.scss';
-import {VscChromeMinimize,  VscChromeRestore, VscChromeMaximize, VscClose } from 'react-icons/vsc';
+import {VscChromeMinimize, VscClose } from 'react-icons/vsc';
 import globalStyles from '../../../../styles/global.module.scss';
-import { setMaximizeMenuListeners } from '../../../../services/system/window/WindowHeaderService';
-import MaximizeOptionsModalComponent, { CustomMaximizeDirection } from '../maximizeOptionsModal/maximizeOptionsModal';
 import { v4 } from 'uuid';
 import { WindowMaximize } from '../../../../constants/system/window/WindowMaximizeEnum';
 import { WindowHeaderProps } from '../../../../types/system/window/WindowHeaderProps';
 import { CONTROL_ICON_SIZE, MAXIMIZE_DIV_ID } from '../../../../constants/system/window/WindowHeaderConts';
+import { HeaderMaximizeButtonComponent } from '../headerMaximizeButton/headerMaximizeButtonComponent';
 
 const HeaderComponent: FC<WindowHeaderProps> = ({
   focused,
@@ -21,29 +20,11 @@ const HeaderComponent: FC<WindowHeaderProps> = ({
 }) => {
 
   const [headerId] = useState<string>(v4());
-  const [showMaximizeMenu, setShowMaximizeMenu] = useState<boolean>(false);
   
   const onMinimize = (event: any) => {
     console.log('minimize');
   };
 
-  useEffect(() => {
-    const cleanupFunction = setMaximizeMenuListeners(`${MAXIMIZE_DIV_ID}_${headerId}`, setShowMaximizeMenu);
-    return () => {
-      if (cleanupFunction) 
-        cleanupFunction();
-    }
-  });
-
-  const onCustomMaximizeClick = (direction: CustomMaximizeDirection) => {
-    setShowMaximizeMenu(false);
-    moveToCustomMaximizeOptionClick(direction);
-  };
-
-  const onMaximizeIconClick = (event: React.MouseEvent) => {
-    setShowMaximizeMenu(false);
-    maximizeWindow(event);
-  }
 
   const getClass = () => {
     return `
@@ -87,27 +68,12 @@ const HeaderComponent: FC<WindowHeaderProps> = ({
         className={styles.maximizeContainer}
         id={MAXIMIZE_DIV_ID + '_' + headerId}
       >
-        <div
-          onClick={onMaximizeIconClick}
-          className={styles.maximizeIcon}
-        >
-          { 
-            maximized === WindowMaximize.Full ? 
-            <VscChromeRestore size={CONTROL_ICON_SIZE}/> :
-            <VscChromeMaximize size={CONTROL_ICON_SIZE}/>
-          }
-        </div>
-
-        { 
-          showMaximizeMenu && 
-          <div
-            className={styles.maximizeOptionModal}
-          > 
-            <MaximizeOptionsModalComponent
-              onCustomMaximizeClick={onCustomMaximizeClick}
-            />
-          </div> 
-        }
+        <HeaderMaximizeButtonComponent
+          maximized={maximized}
+          maximizeButtonHtmlId={MAXIMIZE_DIV_ID + '_' + headerId}
+          moveToCustomMaximizeOptionClick={moveToCustomMaximizeOptionClick}
+          maximizeWindow={maximizeWindow}   
+        />
       </div>
 
       <div
