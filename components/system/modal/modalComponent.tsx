f5f30911843a975c2128ebf68600d52509be93d3
Francois Mahieu <francois.mahieu16@gmail.com>
2023-04-22 12:00:20 -0700

    Create Modal wrapper component

---
+++ components/system/modal/modalComponent.tsx
@@ -0,0 +1,38 @@
+import { FC, Ref, forwardRef, useImperativeHandle, useState } from "react";
+import Modal from 'react-modal';
+
+export interface ModalComponentRef {
+  showModal: (show: boolean) => void;
+}
+
+export const ModalComponent = forwardRef((props, ref: Ref<ModalComponentRef>) => {
+  const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+
+  useImperativeHandle(ref, () => ({
+    showModal
+  }));
+
+  const showModal = (show: boolean) => {
+    setIsOpen(() => show);
+  }
+
+  const style = {
+    content: {
+      top: 'calc(50% - 200px)',
+      left: 'calc(50% - 200px)',
+      height: '200px',
+      width: '200px',
+      backgroundColor: 'white'
+    }
+  }
+
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      style={style}
+    >
+      <div> HELLO</div>
+      <button onClick={() => setIsOpen(false)}>CLOSE</button>
+    </Modal>
+  );
+});
