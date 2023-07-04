23828ebac8221b27d0b2f6b93c31c27917dc4f06
Francois Mahieu <francois.mahieu16@gmail.com>
2023-07-04 13:16:09 -0700

    refactor custom checkbox

--- components/system/custom-checkbox.tsx
+++ components/system/custom-checkbox.tsx
@@ -1,26 +1,5 @@
 import { FC } from "react";
-import styled from "styled-components";
-
-export const StyledCheckbox = styled.div<{ checked: boolean }>`
-  width: 14px;
-  height: 14px;
-
-  border-radius: 0.20em;
-  border: 0.1em solid #626262;
-  background-color: ${({ checked }) => checked ? '#196EBF' : '#F3F3F3'};
-
-  &:hover {
-    background-color: ${({ checked }) => checked ? '#196EBF' : '#EAEAEA'};
-  }
-
-  .clip-path-container {
-    width: 100%;
-    height: 100%;
-    background-color: #E2EDF7;
-    clip-path: polygon(75% 24%, 81% 29%, 43% 77%, 15% 63%, 20% 57%, 40% 67%);
-  }
-`;
-
+import { StyledCheckbox } from "../../styled-components/system/styled-custom-checkbox";
 
 const Checkbox: FC<{
   className?: string,
