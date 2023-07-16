760add812f2afa146a91dda25acd4b06e6bd1430
Francois Mahieu <francois.mahieu16@gmail.com>
2023-07-16 09:57:18 -0700

    Refactor desktop container

--- components/desktop/items-container/DesktopItemsContainer.tsx
+++ components/desktop/items-container/DesktopItemsContainer.tsx
@@ -1,6 +1,6 @@
 import { FC, Fragment, useContext, useEffect, useState } from 'react';
 import { DesktopItem } from '../../../types/desktop/DesktopItem';
-import { explorerItemToDesktopItem, getNewItemName, pathToDesktopItem } from '../../../services/system/desktop/DesktopItemContainerService';
+import { createNewItem, setCurrentItemsFromFileItems } from '../../../services/system/desktop/DesktopItemContainerService';
 import {
 	getSelectedItemsFromSelectionBoxgWithCtrl,
 	moveItemsOnDesktop,
@@ -16,62 +16,30 @@ import { ContextMenuCommandList } from '../../../types/system/context-menu/Conte
 import DesktopItemComponent from '../item/DesktopItem';
 import SelectionBoxComponent from '../../shared/selection-box/SelectionBox';
 import { NewTxtFileCommand } from '../../../System/context-menu-commands/commands/NewTxtFileCommand';
-import { CommonFolderPaths } from '../../../constants/system/file-system/CommonFilePaths';
 import {
 	getCurrentItemNameInPath,
-	getFileExtension,
 	getParentPath,
 	isNewItemNameValid
 } from '../../../services/file-system/FilePathService';
 import { FileSystemContext } from '../../../contexts/FileSystemContext';
 import { ExplorerItem } from '../../../types/system/file/ExplorerItem';
-import { getIconPathByExtension } from '../../../services/IconService';
 import { CreateItemType } from '../../../constants/CreateItemType';
-import { SupportedFileExtension } from '../../../constants/SupportedFileExtension';
 
 const DesktopItemsContainer: FC<{
-	paths: string[];
 	fileItems: ExplorerItem[];
 	onDesktopContextMenuClick: (event: MouseEvent, commands: ContextMenuCommandList) => void;
 	onItemContextMenuClick: (event: MouseEvent) => void;
 	onItemCreated: (path: string) => void;
 	onItemDoubleClick: (item: DesktopItem) => void;
 	onRenameItem: (oldPath: string, newPath: string) => void;
-}> = ({ paths, fileItems, onDesktopContextMenuClick, onItemContextMenuClick, onItemCreated, onItemDoubleClick, onRenameItem }) => {
+}> = ({ fileItems, onDesktopContextMenuClick, onItemContextMenuClick, onItemCreated, onItemDoubleClick, onRenameItem }) => {
 	const fs = useContext(FileSystemContext);
 
 	const [desktopItems, setDesktopItems] = useState<DesktopItem[]>([]);
 
-	// useEffect(() => {
-	// 	const items = paths.map(path => pathToDesktopItem(path, fs.isDirectory(path)));
-	// 	setDesktopItems(() => [...setItemPositions(items, DesktopSortOptions.default)]);
-	// }, [paths]);
-
 	useEffect(() => {
 		setDesktopItems(currentItems => {
-			const updatedItems: DesktopItem[] = [];
-
-			// Update current items that have been modified
-			currentItems.forEach(desktopItem => {
-				const matchingFileItem = fileItems.find(fi => fi.id === desktopItem.id);
-				if (matchingFileItem) {
-					updatedItems.push({
-						fsId: matchingFileItem.id,
-						iconPath: getIconPathByExtension(getFileExtension(matchingFileItem.name)),
-						id: desktopItem.id,
-						left: desktopItem.left,
-						path: CommonFolderPaths.DESKTOP + '/' + matchingFileItem.name,
-						selected: desktopItem.selected,
-						top: desktopItem.top
-					});
-				}
-			});
-
-			// Add new items
-			const newItems = fileItems.filter(fi => !currentItems.find(ci => ci.id === fi.id));
-			updatedItems.push(...newItems.map(i =>
-				explorerItemToDesktopItem(i, CommonFolderPaths.DESKTOP + '/' + i.name, fs.isDirectory(CommonFolderPaths.DESKTOP + '/' + i.name))));
-
+			const updatedItems = setCurrentItemsFromFileItems(fileItems, currentItems, fs.isDirectory);
 			return setItemPositions(updatedItems, DesktopSortOptions.default);
 		});
 	}, [fileItems]);
@@ -129,22 +97,10 @@ const DesktopItemsContainer: FC<{
 
 	const addItem = (top: number, left: number, fileType: CreateItemType) => {
 		setDesktopItems(currentItems => {
-			const isDirectory = fileType === CreateItemType.FOLDER;
-			const newItemName = getNewItemName(fileType, _getExtension(fileType), currentItems);
-			const path = CommonFolderPaths.DESKTOP + '/' + newItemName + _getExtension(fileType);
-			const item = pathToDesktopItem(path, isDirectory);
-			item.left = left;
-			item.top = top;
-
-			onItemCreated(item.path);
-			return [...currentItems, item];
+			const newItem = createNewItem(top, left, fileType, currentItems);
+			onItemCreated(newItem.path);
+			return [...currentItems, newItem];
 		});
-	};
-
-	const _getExtension = (fileType: CreateItemType): string => {
-		if (fileType === 'folder') return '';
-		if (fileType === 'Text Document') return SupportedFileExtension.TXT;
-		return '';
 	};
 
 	// TODO: once changes to the File System triggers a render, this should be handled by the FS context
--- services/system/desktop/DesktopItemContainerService.ts
+++ services/system/desktop/DesktopItemContainerService.ts
@@ -4,8 +4,42 @@ import { getFolderIcon, getIconPathByExtension } from '../../IconService';
 import { getCurrentItemNameInPath, getFileExtension } from '../../file-system/FilePathService';
 import { ExplorerItem } from '../../../types/system/file/ExplorerItem';
 import { CreateItemType } from '../../../constants/CreateItemType';
+import { CommonFolderPaths } from '../../../constants/system/file-system/CommonFilePaths';
+import { SupportedFileExtension } from '../../../constants/SupportedFileExtension';
 
+// eslint-disable-next-line no-useless-escape
 const NEW_FOLDER_NAME_REGEX = (type: CreateItemType) => new RegExp(`New ${type}( \([1-9]+\))?`, 'g');
+
+export const setCurrentItemsFromFileItems = (
+	fileItems: ExplorerItem[],
+	currentItems: DesktopItem[],
+	isDirectory: (path: string) => boolean
+): DesktopItem[] => {
+	const updatedItems: DesktopItem[] = [];
+
+	// Update current items that have been modified
+	currentItems.forEach(desktopItem => {
+		const matchingFileItem = fileItems.find(fi => fi.id === desktopItem.id);
+		if (matchingFileItem) {
+			updatedItems.push({
+				fsId: matchingFileItem.id,
+				iconPath: getIconPathByExtension(getFileExtension(matchingFileItem.name)),
+				id: desktopItem.id,
+				left: desktopItem.left,
+				path: CommonFolderPaths.DESKTOP + '/' + matchingFileItem.name,
+				selected: desktopItem.selected,
+				top: desktopItem.top
+			});
+		}
+	});
+
+	// Add new items
+	const newItems = fileItems.filter(fi => !currentItems.find(ci => ci.id === fi.id));
+	updatedItems.push(...newItems.map(i =>
+		explorerItemToDesktopItem(i, CommonFolderPaths.DESKTOP + '/' + i.name, isDirectory(CommonFolderPaths.DESKTOP + '/' + i.name))));
+
+	return updatedItems;
+};
 
 export const pathToDesktopItem = (path: string, isDirectory: boolean): DesktopItem => {
 	const iconPath = isDirectory
@@ -39,24 +73,35 @@ export const explorerItemToDesktopItem = (explorerItem: ExplorerItem, path: stri
 	};
 };
 
+export const createNewItem = (top: number, left: number, fileType: CreateItemType, currentItems: DesktopItem[]): DesktopItem => {
+	const isDirectory = fileType === CreateItemType.FOLDER;
+	const newItemName = getNewItemName(fileType, getExtension(fileType), currentItems);
+	const path = CommonFolderPaths.DESKTOP + '/' + newItemName;
+	const item = pathToDesktopItem(path, isDirectory);
+	item.left = left;
+	item.top = top;
+
+	return item;
+};
+
 /**
  *
  * @param items
  * @returns either 'New folder' or 'New folder (x)' where x is the next availble number
  * (starting from 2)
  */
-export const getNewItemName = (fileType: CreateItemType, extension: string, items: DesktopItem[]): string => {
+const getNewItemName = (fileType: CreateItemType, extension: string, items: DesktopItem[]): string => {
 	const itemsWithDefaultName = items.filter(item => getCurrentItemNameInPath(item.path).includes(fileType));
 
 	if (itemsWithDefaultName?.length === 0) {
-		return 'New ' + fileType;
+		return 'New ' + fileType + extension;
 	}
 
 	const matches = items.filter(item => NEW_FOLDER_NAME_REGEX(fileType).test(getCurrentItemNameInPath(item.path)));
 
 	// Default name without a number
 	if (!matches.find(item => getCurrentItemNameInPath(item.path) === `New ${fileType}${extension}`)) {
-		return `New ${fileType}`;
+		return `New ${fileType}${extension}`;
 	}
 
 	const numberStartingIndex = `New ${fileType} (`.length;
@@ -71,9 +116,16 @@ export const getNewItemName = (fileType: CreateItemType, extension: string, item
 	let i = 2;
 	for (; i <= digits[digits.length - 1]; i += 1) {
 		if (!digits.includes(i)) {
-			return `New ${fileType} (${i})`;
+			return `New ${fileType} (${i})${extension}`;
 		}
 	}
 
-	return `New ${fileType} (${i})`;
+	return `New ${fileType} (${i})${extension}`;
+};
+
+const getExtension = (fileType: CreateItemType): string => {
+	if (fileType === 'folder') return '';
+	if (fileType === 'Text Document') return '.' + SupportedFileExtension.TXT;
+
+	return '';
 };
