6e167bc96ce38fa3f8d54a93f7749ccf27ad728b
Francois Mahieu <francois.mahieu16@gmail.com>
2023-07-17 20:59:32 -0700

    Creates service and repository to upload analytics

---
+++ services/AnalyticsService.ts
@@ -0,0 +1,26 @@
+import { AnalyticEvents } from '../constants/AnalyticEvents';
+import { saveConnectionFirestore, saveEventFirestore } from '../repositories/AnalyticsFirestore';
+
+const env = process.env.NODE_ENV;
+
+const IP_API = 'https://api.ipify.org/?format=json';
+
+export const saveConnection = async () => {
+  if (env === 'production') {
+    const ipAddress = await getIpAddress();
+    return saveConnectionFirestore(ipAddress);
+  }
+};
+
+export const saveAnalyticsEvent = async (event: AnalyticEvents, payload: { [key: string]: any }) => {
+  // if (env === 'production') {
+    const ipAddress = await getIpAddress();
+    return saveEventFirestore(ipAddress, event, payload);
+  // }
+};
+
+const getIpAddress = (): Promise<string> => {
+  return fetch(IP_API)
+    .then(res => res.json())
+    .then(data => data?.ip || '');
+};
