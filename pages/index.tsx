ef724e5ae738f2e2badf6b0e13a319b7a7f4e1b8
Francois Mahieu <francois.mahieu16@gmail.com>
2023-07-17 20:42:13 -0700

    Test env

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import { ProcessLoader } from '../components/system/ProcessLoader';
 import FileSystemContextProvider from '../contexts/FileSystemContext';
 import ExplorerQuickAccessProvider from '../contexts/ExplorerQuickAccessContext';
 
+const env = process.env.NODE_ENV;
+console.log(env);
+
 const Home: NextPage = () => (
 	<div>
 		<ProcessContextProvider>
