a730a95ef02add00ea992c030625b15765f154be
Francois Mahieu <francois.mahieu16@gmail.com>
2023-07-21 11:09:30 -0700

    Refactor: move taskbarApps logic to service

--- components/taskbar-component/TaskbarApps.tsx
+++ components/taskbar-component/TaskbarApps.tsx
@@ -1,25 +1,16 @@
 import { FC, useContext, useEffect, useState } from 'react';
 import { TaskbarPinnedAppContext } from '../../contexts/TaskbarPinnedAppContext';
 import { ProcessContext } from '../../contexts/ProcessContext';
-import { mergeOpenProcessToApps, mergePinnedAppsToApps } from '../../services/system/taskbar/TaskbarAppService';
+import * as taskbarAppService from '../../services/system/taskbar/TaskbarAppService';
 import { StyledTaskbarApp, TaskbarApp } from './TaskbarApp';
 import { WindowContext } from '../../contexts/WindowContext';
-import { ProcessNameEnum } from '../../System/process/ProcessNameEnum';
-import { GeneralImageCommand } from '../../System/context-menu-commands/commands/GeneralImageCommand';
-import { GeneralIconCommand } from '../../System/context-menu-commands/commands/GeneralIconCommand';
-import { ProcessDirectory } from '../../System/process/ProcessDirectory';
-import { TASKBAR_HEIGHT } from '../../constants/Taskbar';
-import { CONTEXT_MENU_MEDIUM_WIDTH } from '../system/context-menu/ContextMenu';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import Image from 'next/image';
-import { getCurrentItemNameInPath, getFileExtension } from '../../services/file-system/FilePathService';
-import { ProcessDirectoryByExtension } from '../../System/process/ProcessDirectoryByExtension';
-import { DRAG_DROP_DATA_TRANSFER_FIELDS, DRAG_DROP_SOURCE } from '../../constants/DragDrop';
 import { FileSystemContext } from '../../contexts/FileSystemContext';
 import { StyledTaskbarApps } from '../../styled-components/system/taskbar/StyledTaskbarApps';
 import { TaskbarAppType } from '../../types/taskbar/TaskbarAppType';
 
-const PRE_TASKBAR_APP_ID = 'taskbar-app-';
+export const PRE_TASKBAR_APP_ID = 'taskbar-app-';
 
 const TaskbarApps: FC<{}> = () => {
 
@@ -32,126 +23,30 @@ const TaskbarApps: FC<{}> = () => {
 
 	useEffect(() => {
 		setApps(apps => {
-			return mergePinnedAppsToApps(apps, pinnedAppContext.pinnedAppNames);
+			return taskbarAppService.mergePinnedAppsToApps(apps, pinnedAppContext.pinnedAppNames);
 		});
 	}, [pinnedAppContext.pinnedAppNames]);
 
 	useEffect(() => {
 		setApps(apps => {
-			const afterMerge = mergeOpenProcessToApps(apps, windowContext.windows);
+			const afterMerge = taskbarAppService.mergeOpenProcessToApps(apps, windowContext.windows);
 			return [...afterMerge];
 		});
 	}, [windowContext.windows]);
 
 	const handleAppClicked = (appName: string) => {
-		const app = apps.find(app => app.name === appName);
-		if (!app) {
-			console.error(`Error opening app from taskbar: can't find ${appName}.`);
-			return;
-		}
-
-		if (app.open && app.multipleOpen) {
-			// open first available window for that app.
-			// TODO: replace by showing preview of open windows.
-			const window = Object.entries(windowContext.windows).find(([windowId, window]) => window.process.name === appName);
-			if (!window) {
-				return;
-			}
-			windowContext.focusWindow(window[0]);
-		} else if (app.open) {
-			handleMinimizeWindow(appName);
-		} else {
-			processContext.openProcess(appName);
-		}
-	};
-
-	const handleMinimizeWindow = (appName: string) => {
-		const windowToUpdate = Object.entries(windowContext.windows).find(([windowId, { process }]) => process.name === appName);
-		if (!windowToUpdate) {
-			console.error(`Error opening app from taskbar. can't find window to focus for app ${appName}.`);
-			return;
-		}
-
-		// If app is unfocused: focus
-		// else if minimized: focus
-		// eles if focused: minimize
-		const windowId = windowToUpdate[0];
-		const minimized = windowToUpdate[1].state.minimized;
-		const focused = windowToUpdate[1].state.focused;
-
-		if (minimized) {
-			windowContext.unminimizeWindow(windowId);
-		} else if (focused) {
-			windowContext.minimizeWindow(windowId);
-		} else {
-			windowContext.focusWindow(windowId);
-		}
+		taskbarAppService.handleAppClicked(appName, apps, windowContext, processContext);
 	};
 
 	const handleContextMenuClick = (event: MouseEvent, appName: string) => {
 		event.preventDefault();
 		event.stopPropagation();
-
-		const app = apps.find(app => app.name === appName);
-		if (!app) {
-			console.error(`Error opening context menu for app: ${appName}: app not found`);
-			return;
-		}
-
-		const pinCommand = app.pinned ?
-			new GeneralIconCommand(
-				'Unpin from taskbar',
-				require('react-icons/tb').TbPinnedOff,
-				() => pinnedAppContext.removePinnedAppNames(appName)
-			) :
-			new GeneralIconCommand(
-				'Pin to taskbar',
-				require('react-icons/tb').TbPin,
-				() => pinnedAppContext.addPinnedAppNames(appName)
-			);
-
-		const commands = [
-			new GeneralImageCommand(appName, ProcessDirectory[appName].iconPath || '', () => processContext.openProcess(appName)),
-			pinCommand
-		];
-
-		if (app.open) {
-			const processId = Object.values(processContext.processes).find(process => process.name === appName)?.processId || '';
-			const closeCommmand = app.multipleOpen ?
-				new GeneralIconCommand('Close all windows', require('react-icons/gr').GrClose, () => processContext.closeProcessesByName(appName)) :
-				new GeneralIconCommand('Close window', require('react-icons/gr').GrClose, () => processContext.closeProcess(processId));
-			commands.push(closeCommmand);
-		}
-
-		const taskbarAppElement = document.getElementById(PRE_TASKBAR_APP_ID + appName);
-		if (!taskbarAppElement) {
-			return;
-		}
-
-		const elementMiddlewidthFromLeft = taskbarAppElement?.offsetLeft + (taskbarAppElement.offsetWidth / 2);
-		const HEIGHT_OFFSET = 20;
-
-		processContext.openProcess(ProcessNameEnum.CONTEXT_MENU, {
-			commands,
-			left: elementMiddlewidthFromLeft - CONTEXT_MENU_MEDIUM_WIDTH / 2,
-			top: window.innerHeight - TASKBAR_HEIGHT - HEIGHT_OFFSET
-		});
+		taskbarAppService.openContextMenu(appName, apps, pinnedAppContext, processContext);
 	};
 
 	const handleDragEnd = (result: any) => {
-		const startIndex = result?.source?.index;
-		const endIndex = result?.destination?.index;
-
-		if (startIndex === undefined || endIndex === undefined) {
-			// console.error('Error moving taskbar app: start or end index is null.');
-			return;
-		}
-
 		setApps(apps => {
-			const draggedApp = {...apps[startIndex]};
-			apps.splice(startIndex, 1);
-			apps.splice(endIndex, 0, draggedApp);
-			return [...apps];
+			return taskbarAppService.handleDragEnd(result, apps);
 		});
 	};
 
@@ -161,18 +56,7 @@ const TaskbarApps: FC<{}> = () => {
 
 	const onDrop = (event: any) => {
 		event.preventDefault();
-		const source = event?.dataTransfer?.getData(DRAG_DROP_DATA_TRANSFER_FIELDS.SOURCE);
-		const path = event?.dataTransfer?.getData(DRAG_DROP_DATA_TRANSFER_FIELDS.PATH);
-
-		if (source && source === DRAG_DROP_SOURCE.DESKTOP && path) {
-			if (fs.isDirectory(path)){
-				pinnedAppContext.addPinnedAppNames(ProcessNameEnum.EXPLORER);
-				return;
-			}
-
-			const app = ProcessDirectoryByExtension[getFileExtension(getCurrentItemNameInPath(path))];
-			pinnedAppContext.addPinnedAppNames(app);
-		}
+		taskbarAppService.handleItemDrop(event, fs, pinnedAppContext);
 	};
 
 	// Required to let user drop files on the taskbar
--- services/system/taskbar/TaskbarAppService.ts
+++ services/system/taskbar/TaskbarAppService.ts
@@ -1,8 +1,20 @@
+import { GeneralIconCommand } from '../../../System/context-menu-commands/commands/GeneralIconCommand';
+import { GeneralImageCommand } from '../../../System/context-menu-commands/commands/GeneralImageCommand';
 import { ProcessDirectory } from '../../../System/process/ProcessDirectory';
-import { TaskbarAppType } from '../../../components/taskbar-component/TaskbarApps';
+import { ProcessDirectoryByExtension } from '../../../System/process/ProcessDirectoryByExtension';
+import { ProcessNameEnum } from '../../../System/process/ProcessNameEnum';
+import { CONTEXT_MENU_MEDIUM_WIDTH } from '../../../components/system/context-menu/ContextMenu';
+import { PRE_TASKBAR_APP_ID } from '../../../components/taskbar-component/TaskbarApps';
+import { DRAG_DROP_DATA_TRANSFER_FIELDS, DRAG_DROP_SOURCE } from '../../../constants/DragDrop';
 import { IconPaths } from '../../../constants/IconPaths';
-import { Process } from '../../../types/system/processes/Processes';
+import { TASKBAR_HEIGHT } from '../../../constants/Taskbar';
+import { TaskbarPinnedAppContextType } from '../../../contexts/TaskbarPinnedAppContext';
+import { WindowContextType } from '../../../contexts/WindowContext';
+import { FileSystemContextType } from '../../../types/system/file/FileSystemContext';
+import { Process, ProcessContextType } from '../../../types/system/processes/Processes';
 import { Windows } from '../../../types/system/window-manager/WindowManagerState';
+import { TaskbarAppType } from '../../../types/taskbar/TaskbarAppType';
+import { getCurrentItemNameInPath, getFileExtension } from '../../file-system/FilePathService';
 
 export const mergePinnedAppsToApps = (apps: TaskbarAppType[], pinnedAppNames: string[]): TaskbarAppType[] => {
   // remove apps that are not pinned anymore
@@ -85,4 +97,132 @@ const removeClosedProcesses = (apps: TaskbarAppType[], openProcesses: Process[])
     open: processNames.includes(app.name)
   }));
   return closeApps.filter(app => app.pinned || processNames.includes(app.name));
+};
+
+export const openContextMenu = (
+  appName: string,
+  apps: TaskbarAppType[],
+  pinnedAppContext: TaskbarPinnedAppContextType,
+  processContext: ProcessContextType
+) => {
+
+  const app = apps.find(app => app.name === appName);
+  if (!app) {
+    console.error(`Error opening context menu for app: ${appName}: app not found`);
+    return;
+  }
+
+  const pinCommand = app.pinned ?
+    new GeneralIconCommand(
+      'Unpin from taskbar',
+      require('react-icons/tb').TbPinnedOff,
+      () => pinnedAppContext.removePinnedAppNames(appName)
+    ) :
+    new GeneralIconCommand(
+      'Pin to taskbar',
+      require('react-icons/tb').TbPin,
+      () => pinnedAppContext.addPinnedAppNames(appName)
+    );
+
+  const commands = [
+    new GeneralImageCommand(appName, ProcessDirectory[appName].iconPath || '', () => processContext.openProcess(appName)),
+    pinCommand
+  ];
+
+  if (app.open) {
+    const processId = Object.values(processContext.processes).find(process => process.name === appName)?.processId || '';
+    const closeCommmand = app.multipleOpen ?
+      new GeneralIconCommand('Close all windows', require('react-icons/gr').GrClose, () => processContext.closeProcessesByName(appName)) :
+      new GeneralIconCommand('Close window', require('react-icons/gr').GrClose, () => processContext.closeProcess(processId));
+    commands.push(closeCommmand);
+  }
+
+  const taskbarAppElement = document.getElementById(PRE_TASKBAR_APP_ID + appName);
+  if (!taskbarAppElement) {
+    return;
+  }
+
+  const elementMiddlewidthFromLeft = taskbarAppElement?.offsetLeft + (taskbarAppElement.offsetWidth / 2);
+  const HEIGHT_OFFSET = 20;
+
+  processContext.openProcess(ProcessNameEnum.CONTEXT_MENU, {
+    commands,
+    left: elementMiddlewidthFromLeft - CONTEXT_MENU_MEDIUM_WIDTH / 2,
+    top: window.innerHeight - TASKBAR_HEIGHT - HEIGHT_OFFSET
+  });
+};
+
+export const handleDragEnd = (result: any, apps: TaskbarAppType[]): TaskbarAppType[] => {
+  const startIndex = result?.source?.index;
+	const endIndex = result?.destination?.index;
+
+  if (startIndex === undefined || endIndex === undefined) {
+    // console.error('Error moving taskbar app: start or end index is null.');
+    return apps;
+  }
+
+  const draggedApp = {...apps[startIndex]};
+  apps.splice(startIndex, 1);
+  apps.splice(endIndex, 0, draggedApp);
+  return [...apps];
+};
+
+export const handleItemDrop = (event: any, fs: FileSystemContextType, pinnedAppContext: TaskbarPinnedAppContextType) => {
+		const source = event?.dataTransfer?.getData(DRAG_DROP_DATA_TRANSFER_FIELDS.SOURCE);
+		const path = event?.dataTransfer?.getData(DRAG_DROP_DATA_TRANSFER_FIELDS.PATH);
+
+		if (source && source === DRAG_DROP_SOURCE.DESKTOP && path) {
+			if (fs.isDirectory(path)){
+				pinnedAppContext.addPinnedAppNames(ProcessNameEnum.EXPLORER);
+				return;
+			}
+
+			const app = ProcessDirectoryByExtension[getFileExtension(getCurrentItemNameInPath(path))];
+			pinnedAppContext.addPinnedAppNames(app);
+		}
+};
+
+export const handleAppClicked = (appName: string, apps: TaskbarAppType[], windowContext: WindowContextType, processContext: ProcessContextType) => {
+  const app = apps.find(app => app.name === appName);
+  if (!app) {
+    console.error(`Error opening app from taskbar: can't find ${appName}.`);
+    return;
+  }
+
+  if (app.open && app.multipleOpen) {
+    // open first available window for that app.
+    // TODO: replace by showing preview of open windows.
+    const window = Object.entries(windowContext.windows).find(([windowId, window]) => window.process.name === appName);
+    if (!window) {
+      return;
+    }
+    windowContext.focusWindow(window[0]);
+  } else if (app.open) {
+    handleOpenAppClicked(appName, windowContext);
+  } else {
+    processContext.openProcess(appName);
+  }
+};
+
+export const handleOpenAppClicked = (appName: string, windowContext: WindowContextType) => {
+  const windowToUpdate = Object.entries(windowContext.windows).find(([windowId, { process }]) => process.name === appName);
+  if (!windowToUpdate) {
+    console.error(`Error opening app from taskbar. can't find window to focus for app ${appName}.`);
+    return;
+  }
+
+  // If app is unfocused: focus
+  // else if minimized: focus
+  // eles if focused: minimize
+  const windowId = windowToUpdate[0];
+  const minimized = windowToUpdate[1].state.minimized;
+  const focused = windowToUpdate[1].state.focused;
+
+  if (minimized) {
+    windowContext.unminimizeWindow(windowId);
+  } else if (focused) {
+    windowContext.minimizeWindow(windowId);
+  } else {
+    windowContext.focusWindow(windowId);
+  }
 };
